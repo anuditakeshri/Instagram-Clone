bb3d1c8c5705ea99596696863314de268b9acd60
Anudita Keshri <anudita1999@gmail.com>
2021-08-23 12:44:51 +0530

    adding screenshots

--- lib/Screens/loading_screen.dart
+++ lib/Screens/loading_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_app/utils/gradient_text.dart';
 
-
-
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
@@ -11,44 +9,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 5)).then((value) => Navigator.pushReplacementNamed(context, '/feed'));
+    Future.delayed(Duration(seconds: 5))
+        .then((value) => Navigator.pushReplacementNamed(context, '/feed'));
   }
-  
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: SizedBox()
-          ),
-          Image.asset('images/instagram_logo_transparent.png',height: 80.0,),
-          Expanded(
-              child: SizedBox()),
-          Center(child: Text('from',
-          style: TextStyle(
-            fontSize: 15.0,
-            color: Color(0xff868686)
-          ),)),
-          Center(child: Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: GradientText('FACEBOOK', LinearGradient(colors: [Color(0xffBA8247), Color(0xff6B144D)]),TextStyle(
-              fontSize: 20.0,
-              letterSpacing: 2.5
-            ))
-          )),
-          SizedBox(
-            height: 60.0,
-          )
-        ],
-      )
-      );
+        backgroundColor: Colors.black,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: SizedBox()),
+            Image.asset(
+              'images/instagram_logo_transparent.png',
+              height: 80.0,
+            ),
+            Expanded(child: SizedBox()),
+            Center(
+                child: Text(
+              'from',
+              style: TextStyle(fontSize: 15.0, color: Color(0xff868686)),
+            )),
+            Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: GradientText(
+                    'FACEBOOK',
+                    LinearGradient(
+                        colors: [Color(0xffBA8247), Color(0xff6B144D)]),
+                    TextStyle(fontSize: 20.0, letterSpacing: 2.5),
+                  )),
+            ),
+            SizedBox(
+              height: 60.0,
+            )
+          ],
+        ));
   }
 }
